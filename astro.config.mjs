553b544f11a65d2a1ded4c73dcaa2624343650ef
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-09-18 21:30:14 +0200

    config  files
    ajuste de tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import type from "@astrojs/typescrip";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
