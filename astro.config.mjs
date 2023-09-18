65de5763e183ca9a96052991d9580db5f549fb4d
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-09-18 23:27:55 +0200

    fixed issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import type from "@astrojs/typescrip";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
